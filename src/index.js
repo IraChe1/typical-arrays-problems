c7f66757e038a1f2b7edd0f038b0891ffc2af3a9
Ira Che <frontendira@yandex.ru>
2020-10-03 12:40:31 +0300

    feat: all tests work correctly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0
+    } else if (!array.length) {
+        return 0
+    }
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0
+    } else if (!array.length) {
+        return 0
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0
+    } else if (!array.length) {
+        return 0
+    }
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
